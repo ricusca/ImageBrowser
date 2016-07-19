9a2faf7fb1eba67d5622a24b5200d9e7d25a572d
ricusca <robert.icusca@yahoo.com>
2016-07-19 16:29:00 +0300

    Optimized memory usage
    Free pre-allocated buffers after images are downloaded and loaded to textures

--- ImageBrowser/Browser.cpp
+++ ImageBrowser/Browser.cpp
@@ -187,7 +187,7 @@ void Browser::CheckImagesState()
 				f.RetryDownload();
 			}
 			DrawObject(f.GetName());
-			//f.Free(); //optimize for memory..later
+			f.Free(); //optimize for memory..NOW!!!
 		}
 	}
 
